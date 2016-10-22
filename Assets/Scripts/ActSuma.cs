2f22c6a52c5b0551ef17c49959267489ba9a0940
henrycanales7 <henry.javier7@gmail.com>
2016-10-22 15:40:33 -0600

    Initial Commit

---
+++ Assets/Scripts/ActSuma.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+class suma
+{
+	public int[] sumando1 = new int[] {1,2,3,4,5,6,7,8,9,10};
+	public int[] sumando2 = new int[] {1,2,3,4,5,6,7,8,9,10};
+	public string[] signo = new string[]{"+","-"};
+
+	public int num1()
+	{
+		return sumando1[Random.Range(0,11)];
+	}
+	public int num2()
+	{
+		return sumando2[Random.Range(0,11)];
+	}
+	public string sign()
+	{
+		return signo[Random.Range(0,2)];
+	}
+
+	public int Operacion(int n1, int n2, string s)
+	{
+		if (s.Contains ("+")) {
+			return n1 + n2;
+		} else {
+			return n1 - n2;
+		}
+	}
+}
+
+public class ActSuma : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		suma s = new suma ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/RandomNum.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RandomNum : MonoBehaviour {
+
+	public Image UIImagen;
+	public Image UIImagen2;
+	public Image UISigno;
+	public Button Opcion1;
+	public Button Opcion2;
+	public Button Opcion3;
+	// Use this for initialization
+	void Start () {
+		UIImagen = GameObject.Find("A").GetComponent<Image>();
+		int n = Random.Range (1, 11);
+		UIImagen.sprite = Resources.Load<Sprite>("Sprites/"+n);
+		int n2 = Random.Range (1, 11);
+		UIImagen2.sprite = Resources.Load<Sprite>("Sprites/"+n2);
+		int n3 = Random.Range (1, 3);
+		int respuesta = 0;
+		if (n3 == 1) {
+			UISigno.sprite = Resources.Load<Sprite> ("Sprites/+");
+			respuesta = n + n2;
+		} else {
+			UISigno.sprite = Resources.Load<Sprite> ("Sprites/-");
+			respuesta = n - n2;
+			if(respuesta < 0 )
+			{
+				respuesta = respuesta * -1;
+			}
+		}
+
+		int n4 = Random.Range (1, 4);
+		if (n4 == 1) {
+			Opcion1.image.sprite = Resources.Load<Sprite> ("Sprites/" + respuesta);
+		} else {
+			Opcion1.image.sprite = Resources.Load<Sprite> ("Sprites/" + Random.Range (1, 11));
+		}
+		if (n4 == 2) {
+			Opcion2.image.sprite = Resources.Load<Sprite> ("Sprites/" + respuesta);
+		}else {
+			Opcion2.image.sprite = Resources.Load<Sprite> ("Sprites/" + Random.Range (1, 11));
+		}
+		if (n4 == 3) {
+			Opcion3.image.sprite = Resources.Load<Sprite> ("Sprites/" + respuesta);
+		}else {
+			Opcion3.image.sprite = Resources.Load<Sprite> ("Sprites/" + Random.Range (1, 11));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+
+
+
+	
+	}
+}
